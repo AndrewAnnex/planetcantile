5ea2577f5dc4a3bc91b0443ef0633a5f89b15e03
Andrew Annex <ama6fy@virginia.edu>
2022-11-15 10:18:35 -0800

    added moon, already see issues with naming conventions

--- planetcantile/data/generate.py
+++ planetcantile/data/generate.py
@@ -41,16 +41,35 @@ class Tmsparam(object):
         # try to get the geographic_crs
         geographic_crs = self.crs.geodetic_crs
 
+
+gcs_mars_2000_ellipsoid = 'ELLIPSOID["Mars_2000_Sphere_IAU_IAG",3396190,0.0,LENGTHUNIT["metre",1]],ID["ESRI",106971]]'
+gcs_mars_2000_sphere_datum = f'DATUM["D_Mars_2000_Sphere",{gcs_mars_2000_ellipsoid},PRIMEM["Reference_Meridian",0,ANGLEUNIT["degree",0.0174532925199433,ID["EPSG",9122]]]'
+gcs_mars_2000_sphere = CRS(f"""GEOGCRS["GCS_Mars_2000_Sphere",{gcs_mars_2000_sphere_datum},CS[ellipsoidal,2],AXIS["latitude",north,ORDER[1],ANGLEUNIT["degree",0.0174532925199433,ID["EPSG",9122]]],AXIS["longitude",east,ORDER[2],ANGLEUNIT["degree",0.0174532925199433,ID["EPSG",9122]]]]""")
+
+moon_2000_ellipsoid = 'SPHEROID["Moon_2000_IAU_IAG",1737400.0,0.0,LENGTHUNIT["metre",1]]'
+moon_2000_datum = f'DATUM["D_Moon_2000_Sphere",{moon_2000_ellipsoid},PRIMEM["Greenwich",0],UNIT["Decimal_Degree",0.0174532925199433]]'
+moon_2000_sphere = CRS(f'GEOGCS["Moon_2000",{moon_2000_datum},CS[ellipsoidal,2],AXIS["latitude",north,ORDER[1],ANGLEUNIT["degree",0.0174532925199433,ID["EPSG",9122]]],AXIS["longitude",east,ORDER[2],ANGLEUNIT["degree",0.0174532925199433,ID["EPSG",9122]]]]')
+
 # uber tuple of all TmsMatrixSets to generate, if you want something new add it here
 crss = (
     Tmsparam(
-        crs=CRS('GEOGCRS["GCS_Mars_2000_Sphere",DATUM["Mars_2000_(Sphere)",ELLIPSOID["Mars_2000_Sphere_IAU_IAG",3396190,0,LENGTHUNIT["metre",1,ID["EPSG",9001]]]],PRIMEM["Reference_Meridian",0,ANGLEUNIT["degree",0.0174532925199433,ID["EPSG",9122]]],CS[ellipsoidal,2],AXIS["latitude",north,ORDER[1],ANGLEUNIT["degree",0.0174532925199433,ID["EPSG",9122]]],AXIS["longitude",east,ORDER[2],ANGLEUNIT["degree",0.0174532925199433,ID["EPSG",9122]]]]'),
+        crs=gcs_mars_2000_sphere,
         extent=(-180.0, -90.0, 180.0, 90.0),
         title="GCS_Mars_2000_Sphere TileMatrixSet",
         identifier="GCS_Mars_2000_Sphere",
         maxzoom=22,  # set lower for 200mpp MOLA
-        geographic_crs=CRS('GEOGCRS["GCS_Mars_2000_Sphere",DATUM["Mars_2000_(Sphere)",ELLIPSOID["Mars_2000_Sphere_IAU_IAG",3396190,0,LENGTHUNIT["metre",1,ID["EPSG",9001]]]],PRIMEM["Reference_Meridian",0,ANGLEUNIT["degree",0.0174532925199433,ID["EPSG",9122]]],CS[ellipsoidal,2],AXIS["latitude",north,ORDER[1],ANGLEUNIT["degree",0.0174532925199433,ID["EPSG",9122]]],AXIS["longitude",east,ORDER[2],ANGLEUNIT["degree",0.0174532925199433,ID["EPSG",9122]]]]'),
+        geographic_crs=gcs_mars_2000_sphere
     ),
+    Tmsparam(
+        crs=moon_2000_sphere,
+        extent=(-180.0, -90.0, 180.0, 90.0),
+        title="Moon_2000_Sphere TileMatrixSet",
+        identifier="Moon_2000_Sphere",
+        maxzoom=22,
+        geographic_crs=moon_2000_sphere
+    ),
+
+
 )
 
 for tmsp in crss:
