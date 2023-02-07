afafa5ffeb34f17323c6b22aab3c7e11290c3db4
Andrew Annex <ama6fy@virginia.edu>
2023-02-07 08:56:23 -0800

    made tms dict lazy

--- planetcantile/defaults.py
+++ planetcantile/defaults.py
@@ -1,19 +1,9 @@
 import importlib.resources as importlib_resources
 from copy import copy
-from typing import Dict
 
-from morecantile import TileMatrixSet, TileMatrixSets
-
-
-def to_tms(path):
-    with importlib_resources.as_file(path) as src:
-        tms = TileMatrixSet.parse_file(src)
-    return tms
-
+from morecantile import TileMatrixSets
 
 planetary_tms_jsons = importlib_resources.files('planetcantile.data').glob('*.json')
-planetary_tms_dict: Dict[str, TileMatrixSet] = {
-    p.stem: to_tms(p) for p in planetary_tms_jsons
-}
+planetary_tms_path_dict = {p.stem: p for p in planetary_tms_jsons}
 
-planetary_tms = TileMatrixSets(copy(planetary_tms_dict))
+planetary_tms = TileMatrixSets(copy(planetary_tms_path_dict))
