94364793f958895e717b424b09d270a60344f773
Andrew Annex <ama6fy@virginia.edu>
2023-09-14 14:19:11 -0700

    added v1 and v2 folders, will simplify once I can confirm newer pyproj/proj work

--- planetcantile/data/generate.py
+++ planetcantile/data/generate.py
@@ -2,10 +2,12 @@ import json
 import math
 import os
 import urllib.request
+from pathlib import Path
 from dataclasses import dataclass, field, asdict
 import json
 
 from pyproj import CRS, Transformer, Proj
+
 WGS84_CRS = CRS.from_epsg(4326)
 import morecantile
 from morecantile.models import crs_axis_inverted
@@ -17,7 +19,7 @@ matrix_scale_mercator = [1, 1]
 matrix_scale_platecur = [2, 1]
 
 
-def CRS_to_info(crs: CRS)-> tuple[str]:
+def CRS_to_info(crs: CRS) -> tuple[str]:
     """Convert CRS to URI."""
     authority = "EPSG"
     code = "4326"
@@ -38,12 +40,13 @@ def CRS_to_uri(crs: CRS) -> str:
     return f"http://www.opengis.net/def/crs/{authority}/{version}/{code}"
 
 
-def CRS_to_urn(crs: CRS)-> str:
+def CRS_to_urn(crs: CRS) -> str:
     """Convert CRS to URN."""
     authority, version, code = CRS_to_info(crs)
-    if version == '0':
-        version = ''
+    if version == "0":
+        version = ""
     return f"urn:ogc:def:crs:{authority}:{version}:{code}"
+
 
 @dataclass()
 class Tmsparam(object):
@@ -75,81 +78,112 @@ class Tmsparam(object):
         # try to get the geographic_crs
         self.geographic_crs = self.crs.geodetic_crs
 
-# Grab the wkts that mirror the OCG source and parse out just the GeogCRS in a hacktastic way.
-with urllib.request.urlopen('https://raw.githubusercontent.com/pdssp/planet_crs_registry/main/data/result.wkts') as response:
-    resp = response.read().decode(response.headers.get_content_charset())
 
-# Parse all the CRSs from the list
-allcrss = []
-acceptable_projections = ('Equirectangular', 'North Polar', 'South Polar')
-for wkt_str in resp.split(os.linesep + os.linesep):
-    if 'TRIAXIAL' not in wkt_str and 'Westing' not in wkt_str:  # insanely hacky
-        if wkt_str.startswith('GEOGCRS') or any(_ in wkt_str for _ in acceptable_projections):
-            allcrss.append(wkt_str)
+def generate_v1():
+    # Grab the wkts that mirror the OCG source and parse out just the GeogCRS in a hacktastic way.
+    with urllib.request.urlopen(
+        "https://raw.githubusercontent.com/pdssp/planet_crs_registry/main/data/result.wkts"
+    ) as response:
+        resp = response.read().decode(response.headers.get_content_charset())
 
-# Build a dynamic crss list
-crss = []
-for crs in allcrss:
-    crs_obj = CRS(crs)
-    title = crs_obj.name
-    auth = crs_obj.to_authority(min_confidence=25)
-    if auth is not None:
-        authority_version, code = auth
-        authority, version = authority_version.split('_')
-        identifier = f'{authority}_{version}_{code}'
-        geographic_crs = crs_obj.geodetic_crs
-        # Set the extent 
-        clon_180 = "clon = 180" in crs
-        co = crs_obj.coordinate_operation
-        matrix_scale = [2, 1]
-        if co:
-            prj = Proj(crs_obj)
-            if co.name == "North Polar":
-                minx, _ = prj(-90,0)
-                _, miny = prj(0,0)
-                maxx, _ = prj(90,0)
-                _, maxy = prj(180,0)
-                matrix_scale = [1, 1]
-                extent = (minx, miny, maxx, maxy)
-            elif co.name == "South Polar":
-                minx, _ = prj(-90, 0)
-                _, miny = prj(180, 0)
-                maxx, _ = prj(90, 0)
-                _, maxy = prj(0, 0)
-                matrix_scale = [1, 1]
-                extent = (minx, miny, maxx, maxy)
-            elif clon_180:
-                minx, miny = prj(0.0, -90.0)
-                maxx, maxy = prj(359.99999, 90.0)# todo 360.0 wraps to 0 long
-                extent = (minx, miny, math.fabs(minx), maxy)
+    # Parse all the CRSs from the list
+    allcrss = []
+    acceptable_projections = ("Equirectangular", "North Polar", "South Polar")
+    for wkt_str in resp.split(os.linesep + os.linesep):
+        if "TRIAXIAL" not in wkt_str and "Westing" not in wkt_str:  # insanely hacky
+            if wkt_str.startswith("GEOGCRS") or any(
+                _ in wkt_str for _ in acceptable_projections
+            ):
+                allcrss.append(wkt_str)
+
+    # Build a dynamic crss list
+    crss = []
+    for crs in allcrss:
+        crs_obj = CRS(crs)
+        title = crs_obj.name
+        auth = crs_obj.to_authority(min_confidence=25)
+        if auth is not None:
+            authority_version, code = auth
+            authority, version = authority_version.split("_")
+            identifier = f"{authority}_{version}_{code}"
+            geographic_crs = crs_obj.geodetic_crs
+            # Set the extent
+            clon_180 = "clon = 180" in crs
+            co = crs_obj.coordinate_operation
+            matrix_scale = [2, 1]
+            if co:
+                prj = Proj(crs_obj)
+                if co.name == "North Polar":
+                    minx, _ = prj(-90, 0)
+                    _, miny = prj(0, 0)
+                    maxx, _ = prj(90, 0)
+                    _, maxy = prj(180, 0)
+                    matrix_scale = [1, 1]
+                    extent = (minx, miny, maxx, maxy)
+                elif co.name == "South Polar":
+                    minx, _ = prj(-90, 0)
+                    _, miny = prj(180, 0)
+                    maxx, _ = prj(90, 0)
+                    _, maxy = prj(0, 0)
+                    matrix_scale = [1, 1]
+                    extent = (minx, miny, maxx, maxy)
+                elif clon_180:
+                    minx, miny = prj(0.0, -90.0)
+                    maxx, maxy = prj(359.99999, 90.0)  # todo 360.0 wraps to 0 long
+                    extent = (minx, miny, math.fabs(minx), maxy)
+                else:
+                    minx, miny = prj(-180, -90)
+                    maxx, maxy = prj(180, 90)
+                    extent = (minx, miny, maxx, maxy)
             else:
-                minx, miny = prj(-180, -90)
-                maxx, maxy = prj(180, 90)
-                extent = (minx, miny, maxx, maxy)
+                # if clon == 180 we have a 0-360 longitude crs
+                extent = (
+                    (0.0, -90.0, 360.0, 90.0)
+                    if clon_180
+                    else (-180.0, -90.0, 180.0, 90.0)
+                )
+            tmsp = Tmsparam(
+                crs=crs_obj,
+                extent=extent,
+                extent_crs=crs_obj,
+                title=title,
+                identifier=identifier,
+                matrix_scale=matrix_scale,
+                geographic_crs=geographic_crs,
+            )
+            crss.append(tmsp)
         else:
-            # if clon == 180 we have a 0-360 longitude crs
-            extent = (0.0, -90.0, 360.0, 90.0) if clon_180 else (-180.0, -90.0, 180.0, 90.0)
-        tmsp = Tmsparam(
-            crs=crs_obj,
-            extent=extent,
-            extent_crs=crs_obj,
-            title=title,
-            identifier=identifier,
-            matrix_scale=matrix_scale,
-            geographic_crs=geographic_crs
-        )
-        crss.append(tmsp)
-    else:
-        pass
-        #print(f'Could not find authority for {crs_obj.to_wkt()}')
+            pass
+            # print(f'Could not find authority for {crs_obj.to_wkt()}')
 
-for tmsp in crss:
-    # create the tms object
-    tms = morecantile.TileMatrixSet.custom(**asdict(tmsp))
-    tmsj = tms.dict(exclude_none=True)
-    # Include URN to the planetary projections; _geographic_crs is needed by downstream libs, e.g., morecantile 
-    tmsj['supportedCRS'] = tmsj['boundingBox']['crs'] = CRS_to_urn(tmsj['boundingBox']['crs'])
-    tmsj['_geographic_crs'] = CRS_to_urn(tms._geographic_crs)
-    with open(f'./{tmsp.identifier}.json', 'w') as dst:
-        json.dump(tmsj, dst, indent=4, ensure_ascii=True)
-        print(f'wrote {dst.name}')
+    for tmsp in crss:
+        # create the tms object
+        tms = morecantile.TileMatrixSet.custom(**asdict(tmsp))
+        tmsj = tms.dict(exclude_none=True)
+        # Include URN to the planetary projections; _geographic_crs is needed by downstream libs, e.g., morecantile
+        tmsj["supportedCRS"] = tmsj["boundingBox"]["crs"] = CRS_to_urn(
+            tmsj["boundingBox"]["crs"]
+        )
+        tmsj["_geographic_crs"] = CRS_to_urn(tms._geographic_crs)
+        with open(f"./v1/{tmsp.identifier}.json", "w") as dst:
+            json.dump(tmsj, dst, indent=4, ensure_ascii=True)
+            print(f"wrote {dst.name}")
+
+
+def generate_v2_from_v1():
+    tms_v1_jsons = Path("./v1/").glob("*.json")
+    for v1tms in tms_v1_jsons:
+        with open(v1tms) as f:
+            data = json.load(f)
+        v2tms: morecantile.TileMatrixSet = morecantile.TileMatrixSet.from_v1(data)
+        v2tms.orderedAxes = [_.abbrev for _ in CRS.from_user_input(v2tms.crs).axis_info]
+        with open(f"./v2/{v2tms.id}.json", "w") as dst:
+            json.dump(v2tms.dict(exclude_none=True), dst, indent=4, ensure_ascii=True)
+            print(f"wrote {dst.name}")
+
+
+if __name__ == "__main__":
+    try:
+        generate_v1()
+    finally:
+        generate_v2_from_v1()
