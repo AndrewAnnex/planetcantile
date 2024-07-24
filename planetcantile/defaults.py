e33ec4a7e2594dbea5a19dcafbf1bd0a5532f660
Dr. Andrew Annex <annex@seti.org>
2024-07-24 14:58:04 -0700

    added lru cache for tms

--- planetcantile/defaults.py
+++ planetcantile/defaults.py
@@ -1,9 +1,17 @@
 import importlib.resources as importlib_resources
 from copy import copy
+from functools import lru_cache
 
 from morecantile import TileMatrixSets
 
-planetary_tms_jsons = importlib_resources.files("planetcantile.data.v2").glob("*.json")
-planetary_tms_path_dict = {p.stem: p for p in planetary_tms_jsons}
+# todo how slow is this going to be? maybe worth a lru cache and defaulting to v3
 
-planetary_tms = TileMatrixSets(copy(planetary_tms_path_dict))
+@lru_cache
+def get_jsons(v: int = 3):
+    return importlib_resources.files("planetcantile.data.v{v}").glob("*.json")
+
+@lru_cache
+def get_planetcantile_tms(v: int = 3):
+    return TileMatrixSets({p.stem: p for p in get_jsons(v)})
+
+planetary_tms = get_planetcantile_tms(v=3)
